75518a27353af9df3984bfc9c439708f103083fb
geeeh <godwingitonga89@gmail.com>
2017-05-09 14:16:46 -0700

    third day commit

---
+++ MaxMin.py
@@ -0,0 +1,24 @@
+def maxmin(vars):
+    y=[]
+    m=vars[0]
+    z=vars[0]
+    if not isinstance(vars,list):
+        return "only lists allowed"
+
+    else:
+        for x in vars:
+            if x<z:
+                z=x
+                
+            elif x>m:
+                m=x
+
+            else:
+                pass
+
+        y.append([z,m])
+
+    return y
+print(maxmin([1,2,3,4]))
+            
+
---
+++ WordCount.py
@@ -0,0 +1,17 @@
+def word_counter(y):
+    
+    if not isinstance(y, str):
+        return("only string allowed")
+    else:
+        g = y.lower().split()
+        v = {}
+        for x in g:
+            if x in v:
+                v[x] += 1
+            else:
+                v[x] = 1
+        return v
+    
+        
+
+print(word_counter("ho many many Many"))
