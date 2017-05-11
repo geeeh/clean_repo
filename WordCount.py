21636d620b48b25e14098376f08bcef90e808eeb
geeeh <godwingitonga89@gmail.com>
2017-05-11 15:47:44 +0300

    wordcount modified

--- WordCount.py
+++ WordCount.py
@@ -1,9 +1,10 @@
-def word_counter(y):
+def words(y):
     
-    if not isinstance(y, str):
-        return("only string allowed")
+    if not isinstance(y, (str, int)):
+        return("only string and numbers allowed")
     else:
-        g = y.lower().split()
+        # y=str(y)
+        g = y.split()
         v = {}
         for x in g:
             if x in v:
@@ -14,4 +15,88 @@ def word_counter(y):
     
         
 
-print(word_counter("ho many many Many"))
+print(words("ho  many : many\n Many 1 2 javascript!!&@$%^&"))
+# import unittest
+
+# class TestWordCounts(unittest.TestCase):
+
+#     """
+#         Counts the occurrences or characters in a word
+#     """
+
+#     def test_word_occurance1(self):
+#         self.assertDictEqual(
+#             {'word': 1},
+#             words('word'),
+#             msg='should count one word'
+#         )
+
+#     def test_word_occurance2(self):
+#         self.assertDictEqual(
+#             {'one': 1, 'of': 1, 'each': 1},
+#             words("one of each"),
+#             msg='should count one of each'
+#         )
+
+#     def test_word_occurance3(self):
+#         self.assertDictEqual(
+#             {'one': 1, 'fish': 4, 'two': 1, 'red': 1, 'blue': 1},
+#             words("one fish two fish red fish blue fish"),
+#             msg='should count multiple occurrences'
+#         )
+
+#     def test_word_occurance4(self):
+#         self.assertDictEqual(
+#             {'car': 1,
+#                 ":": 2,
+#                 'carpet': 1,
+#                 'as': 1,
+#                 'java': 1,
+#                 'javascript!!&@$%^&': 1
+#              },
+#             words('car : carpet as java : javascript!!&@$%^&'),
+#             msg='should include punctuation'
+#         )
+
+#     def test_word_occurance5(self):
+#         self.assertDictEqual(
+#             {'testing': 2, 1: 1, 2: 1},
+#             words('testing 1 2 testing'),
+#             msg='should include numbers'
+#         )
+
+#     def test_word_occurance6(self):
+#         self.assertDictEqual(
+#             {'go': 1, 'Go': 1, 'GO': 1},
+#             words('go Go GO'),
+#             msg='should respect case'
+#         )
+
+#     def test_word_occurance7(self):
+#         self.assertDictEqual(
+#             {"¡Hola!": 1, "¿Qué": 1, "tal?": 1, "Привет!": 1},
+#             words('¡Hola! ¿Qué tal? Привет!'),
+#             msg='should count international characters properly'
+#         )
+
+#     def test_word_occurance8(self):
+#         self.assertDictEqual(
+#             {'hello': 1, 'world': 1},
+#             words('hello\nworld'),
+#             msg='should not count multilines'
+#         )
+
+#     def test_word_occurance9(self):
+#         self.assertDictEqual(
+#             {'hello': 1, 'world': 1},
+#             words('hello\tworld'),
+#             msg='should not count tabs'
+#         )
+
+#     def test_word_occurance0(self):
+#         self.assertDictEqual(
+#             {'hello': 1, 'world': 1},
+#             words('hello  world'),
+#             msg='should count multiple spaces as one'
+#         )
+# unittest.main()
