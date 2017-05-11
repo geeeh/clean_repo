48cb3d039e1042b65b275e57550b4dfdf5f1a7c2
geeeh <godwingitonga89@gmail.com>
2017-05-11 19:11:43 +0300

    getting modification

--- DataTypes.py
+++ DataTypes.py
@@ -23,29 +23,29 @@ def data_type(my_vars):
     else:
         return "please input data"
 
-import unittest
+# import unittest
 
-class DataTypeTestCase(unittest.TestCase):
+# class DataTypeTestCase(unittest.TestCase):
     
-  def test_none_type(self):
-    self.assertEqual('no value', data_type(None))
+#   def test_none_type(self):
+#     self.assertEqual('no value', data_type(None))
 
-  def test_array_type(self):
-    self.assertEqual(3, data_type([1, 2, 3]))
+#   def test_array_type(self):
+#     self.assertEqual(3, data_type([1, 2, 3]))
 
-  def test_small_array_type(self):
-    self.assertEqual(None, data_type([1, 2]))
+#   def test_small_array_type(self):
+#     self.assertEqual(None, data_type([1, 2]))
 
-  def test_small_booleans_type(self):
-    self.assertEqual(True, data_type(True))
+#   def test_small_booleans_type(self):
+#     self.assertEqual(True, data_type(True))
 
-  def test_small_integer_type(self):
-    self.assertEqual('less than 100', data_type(3))
+#   def test_small_integer_type(self):
+#     self.assertEqual('less than 100', data_type(3))
 
-  def test_large_integer_type(self):
-    self.assertEqual('more than 100', data_type(200))
+#   def test_large_integer_type(self):
+#     self.assertEqual('more than 100', data_type(200))
   
-  def test_str_type(self):
-    self.assertEqual(6, data_type('andela'))
+#   def test_str_type(self):
+#     self.assertEqual(6, data_type('andela'))
 
-unittest.main()
+# unittest.main()
