c56e198b419ac4e3a80c006a5f5684c48d7dcddd
geeeh <godwingitonga89@gmail.com>
2017-05-11 21:23:41 +0300

    adding circle_calculations program modified

---
+++ Circle_circulations.py
@@ -0,0 +1,26 @@
+class Circle():
+    def __init__(self, radius):
+        self.radius=radius
+        self.diameter=self.radius*2
+
+    def Area(self):
+        return(3.142*self.radius**2)
+
+    def Circumference(self):
+        return(3.142*self.diameter)
+
+class my_circle(Circle):
+    def __init__(self, radius, color):
+        Circle.__init__(self, radius)
+        self.color=color
+        
+
+cir= my_circle(7,"blue")
+print(cir.color)
+
+        
+        
+
+
+
+
